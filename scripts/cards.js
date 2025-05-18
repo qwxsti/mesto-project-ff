a5a9ff3858aaa3811b874b63d1b8003cc7c4885b
qwxsti <qwxsti@gmail.com>
2025-05-18 19:18:47 +0300

    Добавлены альтернативные тексты для изображений карточек и добавлен код для создания и удаления карточек (1 сдача)

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,32 @@ const initialCards = [
     {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      imgAlt: "Горы Архыза: небольшое озеро с островком посреди долины, окруженной скалами, снегом и альпийскими лугами",
     },
     {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      imgAlt: "Зимняя река в Челябинской области, извивающаяся среди покрытых снегом скалистых берегов и леса",
     },
     {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      imgAlt: "Вечерний городской пейзаж Иваново: плотная застройка многоэтажных жилых домов со светящимися окнами.",
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      imgAlt: "Камчатский пейзаж: заснеженный вулкан, возвышающийся над каменистой тундрой с низкорослой растительностью и светлыми корнями",
+
     },
     {
       name: "Холмогорский район",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      imgAlt: "Железнодорожные пути, уходящие вдаль сквозь густой лес Холмогорского района под вечерним небом",
     },
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      imgAlt: "Зимний Байкал: скалистый утес, выступающий в замерзшее озеро с битым льдом",
     }
 ];
